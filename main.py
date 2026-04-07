883a299e50901a8793593aeaa1c7f0834550b148
dirklangohr <91342718+dirklangohr@users.noreply.github.com>
2026-04-07 22:30:37 +0200

    Add distribution plots for training data of all 4 experiments

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils.config import Config
 from data_handler import DataHandler
 from classificators.dummy_classifier import DummyClassifier
 from classificators.random_forest_classifier import RandomForestClassifierSK
-from utils.utils import calculate_mcc_multilabel, plot_per_class_confusion
+from utils.utils import calculate_mcc_multilabel, plot_per_class_confusion, plot_class_distribution
 
 if __name__ == '__main__':
 
@@ -39,14 +39,14 @@ if __name__ == '__main__':
         train, val, test, target_vals = datahandler.get_data_loaders()
 
         # just to get an insight into the data
-        #plot_class_distribution(train[1], target_vals)
-        #plot_class_distribution(val[1], target_vals)
-        #plot_class_distribution(test[1], target_vals)
+        # plot_class_distribution(train[1], target_vals)
+        # plot_class_distribution(val[1], target_vals)
+        # plot_class_distribution(test[1], target_vals)
 
         try:
 
             #model = DummyClassifier(target_vals)
-            model = RandomForestClassifierSK(target_vals)
+            # model = RandomForestClassifierSK(target_vals)
             ### INSERT YOUR MODEL HERE ###
             # model = MyAwesomeModel(...)
 
@@ -55,9 +55,9 @@ if __name__ == '__main__':
             print("Training model...")
             # Note: Any kind of preprocessing, data augmentation or feature engineering should be done within the model.train() function
             # so it's capsuled within the model class (see RandomForestClassifierSK for an example)
-            model.train(train, val)
+            # model.train(train, val)
             print("Evaluating model...")
-            predicted_y = model.predict(test[0])
+            # predicted_y = model.predict(test[0])
 
             # optional, for more insight, plot the per-class-confusion-matrix for the test set
             # plot_per_class_confusion(test[1], predicted_y, target_vals)
@@ -67,8 +67,8 @@ if __name__ == '__main__':
             # Since we average over classes, it can happen that some classes have a
             # negative MCC while others have a positive MCC, resulting that scores balance each other out
             # Check your individual scores to see if they are reasonable
-            test_mcc = calculate_mcc_multilabel(predicted_y, test[1])
-            test_mccs.append(test_mcc)
+            # test_mcc = calculate_mcc_multilabel(predicted_y, test[1])
+            # test_mccs.append(test_mcc)
 
         except Exception as e:
             print(f"Fold {fold} failed with error: {e}")
