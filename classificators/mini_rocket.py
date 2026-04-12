8ba4db561e44f7076de1068000bb0008f153c3fa
dirklangohr <91342718+dirklangohr@users.noreply.github.com>
2026-04-12 17:35:38 +0200

    Added MiniRocket classifier

---
+++ classificators/mini_rocket.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sktime.pipeline import make_pipeline
+from sktime.transformations.panel.rocket import MiniRocketMultivariate
+
+
+class MiniRocketClassifier:
+    def __init__(self):
+        minirocket = MiniRocketMultivariate(num_kernels=1000, random_state=42)
+
+        sgd_logic = SGDClassifier(
+            loss='log_loss',
+            penalty='l2',
+            class_weight='balanced',
+            max_iter=1000,
+            tol=1e-3,
+            random_state=42
+        )
+
+        self.rocket_pipeline = make_pipeline(minirocket, StandardScaler(with_mean=False), sgd_logic)
+
+    def train(self, train, val):
+        X_train, y_train = train
+        X_val, y_val = val
+        self.rocket_pipeline.fit(X_train, y_train)
+        return self.rocket_pipeline.predict(X_val)
+
+    def predict(self, X: np.ndarray):
+        return self.rocket_pipeline.predict(X).to_numpy()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 import numpy as np
 
+from classificators.mini_rocket import MiniRocketClassifier
 from utils.config import Config
 from utils.plotting import plot_gyroscope_3d, plot_acceleration_3d
 from data_handler import DataHandler
@@ -23,13 +24,14 @@ if __name__ == '__main__':
 
     val_mccs = []
     test_mccs = []
+    train_mccs = []
     lr_histories_by_fold = {}
 
     # load data
     datahandler = DataHandler(config=config)
 
     # Leave-one-out: EXPERIMENT_ID = 1..4
-    for fold in range(1, 5):
+    for fold in range(1, 2):
         print(f"\n--- Fold {fold}/4 | EXPERIMENT_ID={fold} ---")
         val_id = fold + 1 if fold < 4 else 1
 
@@ -37,12 +39,17 @@ if __name__ == '__main__':
         # validation hat to be different from test
         datahandler.config.data.validation_experiment_id = val_id
 
-        # train, val, test, target_vals = datahandler.get_data_loaders()
-        train, val, test, target_vals = datahandler.load_dataframes()
-        plot_acceleration_3d(train.sample(n=5000))
-        plot_gyroscope_3d(train.sample(n=5000))
+        skipsize: int = 25
+        # train, val, test, target_vals = datahandler.get_data_loaders(skipisze)
+        # train, val, test, target_vals = datahandler.load_dataframes(skipsize)
+        train, val, test, target_vals = datahandler.get_minirocket_data(skipsize)
+        print(f"data shape: {train[0].shape}")
+        print(f"label shape: {train[1].shape}")
+
 
         # just to get an insight into the data
+        # plot_acceleration_3d(train.sample(n=5000))
+        # plot_gyroscope_3d(train.sample(n=5000))
         # plot_class_distribution(train[1], target_vals)
         # plot_class_distribution(val[1], target_vals)
         # plot_class_distribution(test[1], target_vals)
@@ -52,16 +59,17 @@ if __name__ == '__main__':
             #model = DummyClassifier(target_vals)
             # model = RandomForestClassifierSK(target_vals)
             ### INSERT YOUR MODEL HERE ###
-            # model = MyAwesomeModel(...)
+            model = MiniRocketClassifier()
 
             # Here, we use the validation set to follow good machine learning practice, which is particularly relevant for evaluation during training.
             # However, the validation data can also be incorporated directly into the model training if necessary.
             print("Training model...")
             # Note: Any kind of preprocessing, data augmentation or feature engineering should be done within the model.train() function
             # so it's capsuled within the model class (see RandomForestClassifierSK for an example)
-            # model.train(train, val)
+            train_y_hat = model.train(train, val)
             print("Evaluating model...")
-            # predicted_y = model.predict(test[0])
+            test_y_hat = model.predict(test[0])
+
 
             # optional, for more insight, plot the per-class-confusion-matrix for the test set
             # plot_per_class_confusion(test[1], predicted_y, target_vals)
@@ -71,8 +79,10 @@ if __name__ == '__main__':
             # Since we average over classes, it can happen that some classes have a
             # negative MCC while others have a positive MCC, resulting that scores balance each other out
             # Check your individual scores to see if they are reasonable
-            # test_mcc = calculate_mcc_multilabel(predicted_y, test[1])
-            # test_mccs.append(test_mcc)
+            test_mcc = calculate_mcc_multilabel(test_y_hat, test[1])
+            test_mccs.append(test_mcc)
+            train_mcc = calculate_mcc_multilabel(train_y_hat, train[1])
+            train_mccs.append(test_mcc)
 
         except Exception as e:
             print(f"Fold {fold} failed with error: {e}")
@@ -80,5 +90,7 @@ if __name__ == '__main__':
         print("--- End of Fold ---")
 
     avg_mcc = sum(test_mccs) / len(test_mccs)
-    print("Scores for each run: ", test_mccs)
-    print("\nTotal score:", avg_mcc)
+    print("Training scores for each run: ", train_mccs)
+    print("Test scores for each run: ", test_mccs)
+    print("\nTotal training score: ", sum(train_mccs) / len(train_mccs))
+    print("Total test score:", avg_mcc)
